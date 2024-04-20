2177156dd3707bd34a79d8dd2bbbb961fbee2ebc
Youzhi Wang <yw4196@columbia.edu>
2024-04-20 02:11:37 -0400

    added a shortcut for opening files in obisidan

--- init.lua
+++ init.lua
@@ -56,4 +56,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Obsidian settings
+		vim.cmd('noremap <leader>oo :ObsidianOpen<cr>')
 end
