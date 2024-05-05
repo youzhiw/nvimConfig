5698a062ccfec5d3af1ff3c7a6837a3e151f337a
Youzhi Wang <yw4196@columbia.edu>
2024-05-05 01:49:01 -0400

    changed the clipboard setting to be faster, change python lsp to pyright, changed back to nvim-tree

--- lua/youzhi/plugins/lsp-config.lua
+++ lua/youzhi/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "pylsp"}
+                ensure_installed = {"lua_ls", "pylsp", "pyright"}
             })
         end
     },
@@ -17,7 +17,7 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.pylsp.setup({})
+            lspconfig.pyright.setup({})
             lspconfig.lua_ls.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
--- lua/youzhi/vim-options.lua
+++ lua/youzhi/vim-options.lua
@@ -41,6 +41,18 @@ vim.cmd('nmap j gj')
 vim.cmd('nmap k gk')
 
 -- sync system clipboard
+vim.g.clipboard = {
+ name = "xsel",
+ copy = {
+  ["+"] = "xsel --nodetach -i -b",
+  ["*"] = "xsel --nodetach -i -p",
+ },
+ paste = {
+  ["+"] = "xsel  -o -b",
+  ["*"] = "xsel  -o -b",
+ },
+ cache_enabled = 1,
+}
 vim.opt.clipboard = 'unnamedplus'
 
 -- search ignoring case
