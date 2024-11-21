522676e08c368a4aa00af14a7e38607936eec3d5
Youzhi Wang <youzhiwang2001@gmail.com>
2024-11-21 15:13:25 +0800

    added lsp for ts, js

--- lua/youzhi/plugins/lsp-config.lua
+++ lua/youzhi/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "pylsp", "pyright"}
+                ensure_installed = {"lua_ls", "pyright"}
             })
         end
     },
@@ -17,8 +17,22 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
+            -- Python LSP
             lspconfig.pyright.setup({})
+
+            -- Lua LSP
             lspconfig.lua_ls.setup({})
+
+            -- TypeScript and JavaScript LSP
+            lspconfig.tsserver.setup({
+                on_attach = function(client, bufnr)
+                    -- Disable tsserver's formatting (optional if using Prettier)
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentRangeFormattingProvider = false
+                end
+            })
+
+            -- Keybindings for LSP actions
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
