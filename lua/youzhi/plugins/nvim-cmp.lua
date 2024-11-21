8dfd776143db85f32ef1e20b180341f2bbf1f927
Youzhi Wang <youzhiwang2001@gmail.com>
2024-11-21 15:55:31 +0800

    added snippets support

--- lua/youzhi/plugins/nvim-cmp.lua
+++ lua/youzhi/plugins/nvim-cmp.lua
@@ -1,13 +1,16 @@
 local M = {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"saadparwaiz1/cmp_luasnip",
-		"L3MON4D3/LuaSnip",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/vim-vsnip",
+        "hrsh7th/vim-vsnip-integ"
 	},
 }
 
@@ -26,9 +29,9 @@ M.config = function()
 			-- documentation = cmp.config.window.bordered(),
 		},
 		mapping = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
+			-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-b>"] = cmp.mapping.complete(),
 			["<C-e>"] = cmp.mapping.abort(),
 			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		}),
@@ -36,6 +39,7 @@ M.config = function()
 			{ name = "nvim_lsp" },
 			{ name = "nvim_lua" },
 			{ name = "luasnip" }, -- For luasnip users.
+            { name = "vsnip" },
 			-- { name = "orgmode" },
 		}, {
 			{ name = "buffer" },
